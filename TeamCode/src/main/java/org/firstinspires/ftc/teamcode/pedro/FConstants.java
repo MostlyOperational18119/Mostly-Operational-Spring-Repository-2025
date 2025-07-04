e70ce488f37c4f13afeea1460cb8ce90b48a2844
TheComputerToucher <damien.brown15@outlook.com>
2025-07-03 20:12:11 -0700

    Tuning :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/FConstants.java
@@ -2,13 +2,34 @@ package org.firstinspires.ftc.teamcode.pedro;
 
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.localization.Localizers;
+import com.pedropathing.util.CustomFilteredPIDFCoefficients;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class FConstants {
     static {
         // Select our localizer
-        FollowerConstants.localizers = Localizers.THREE_WHEEL;
+        FollowerConstants.localizers = Localizers.PINPOINT;
 
         // We can change the value of any variable/constant of FollowerConstants.
         FollowerConstants.mass = 14.515; // In kg
+
+        FollowerConstants.leftFrontMotorName = "motorFL";
+        FollowerConstants.leftRearMotorName = "motorBL";
+        FollowerConstants.rightFrontMotorName = "motorFR";
+        FollowerConstants.rightRearMotorName = "motorBR";
+
+        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
+        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+
+        // Tuning D:
+        FollowerConstants.xMovement = 67.8989;
+        FollowerConstants.yMovement = 47.0148;
+
+        FollowerConstants.forwardZeroPowerAcceleration = -33.49;
+        FollowerConstants.lateralZeroPowerAcceleration = -74.9233;
+
+        FollowerConstants.drivePIDFCoefficients = new CustomFilteredPIDFCoefficients(0.012, 0.0, 0.0001, 0.6, 0.0);
     }
 }
