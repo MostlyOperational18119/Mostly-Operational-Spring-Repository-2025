87c4dc3c0f0853f6cb539c630cb7334f35100dc5
MostlyOperational18119 <64237204+MostlyOperational18119@users.noreply.github.com>
2025-09-20 10:14:37 -0700

    push

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AprilTagTest.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@TeleOp(name = "AprilTag Detection", group = "Vision")
+public class AprilTagTest extends LinearOpMode {
+
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+
+    @Override
+    public void runOpMode() {
+        aprilTag = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagOutline(true)
+                .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam"));
+        builder.enableLiveView(true);
+        builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+        builder.setAutoStopLiveView(false);
+        builder.addProcessor(aprilTag);
+        visionPortal = builder.build();
+
+
+        while (opModeIsActive()) {
+            if (gamepad1.dpad_down) {
+                visionPortal.stopStreaming();
+            } else if (gamepad1.dpad_up) {
+                visionPortal.resumeStreaming();
+            }
+
+            List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+            telemetry.addData("# AprilTags Detected", currentDetections.size());
+            for (AprilTagDetection detection : currentDetections) {
+                telemetry.addData("ID: ", detection.id);
+            }
+            telemetry.update();
+
+            sleep(20);
+        }
+    }
+}
