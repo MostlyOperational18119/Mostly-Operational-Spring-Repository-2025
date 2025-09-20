c2476bbe6c287edd46929c43aeec5b1482ec6f66
18119 Mostly Operational <64237204+MostlyOperational18119@users.noreply.github.com>
2025-09-20 11:21:36 -0700

    Stuf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alex/AprilTagAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alex/AprilTagAuto.java
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.teamcode.alex;
 
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.pedro.FConstants;
+import org.firstinspires.ftc.teamcode.pedro.LConstants;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
@@ -17,12 +23,34 @@ import java.util.List;
 
 public class AprilTagAuto extends LinearOpMode {
 
+    Pose startPosition = new Pose(0, 0, Math.toRadians(0));
+    Pose leftPosition = new Pose(12, 0, Math.toRadians(0));
+    Pose rightPosition = new Pose(-12, 0, Math.toRadians(0));
+    Pose backPosition = new Pose(0, -12, Math.toRadians(0));
     private VisionPortal visionPortal;
     private AprilTagProcessor apriltag;
     private int AprilTagId  = -1;
+    Follower follower;
+    PathChain left, right, back;
     @Override
 
     public void runOpMode() {
+        follower = new Follower(hardwareMap, FConstants.class, LConstants.class);
+
+        left = follower.pathBuilder()
+            .addPath(new BezierLine(startPosition, leftPosition))
+            .setLinearHeadingInterpolation(startPosition.getHeading(), leftPosition.getHeading())
+            .build();
+
+        right = follower.pathBuilder()
+                .addPath(new BezierLine(startPosition, rightPosition))
+                .setLinearHeadingInterpolation(startPosition.getHeading(), rightPosition.getHeading())
+                .build();
+
+        back = follower.pathBuilder()
+                .addPath(new BezierLine(startPosition, backPosition))
+                .setLinearHeadingInterpolation(startPosition.getHeading(), backPosition.getHeading())
+                .build();
 
         apriltag = new AprilTagProcessor.Builder()
                 .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
@@ -53,8 +81,18 @@ public class AprilTagAuto extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
+            follower.update();
 
-
+            if (!follower.isBusy()) {
+                switch (AprilTagId) {
+                    case 21:
+                        follower.followPath(left);
+                    case 22:
+                        follower.followPath(right);
+                    case 23:
+                        follower.followPath(back);
+                }
+            }
         }
 
 
